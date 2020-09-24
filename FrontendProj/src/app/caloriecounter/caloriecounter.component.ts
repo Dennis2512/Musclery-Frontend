8708d335b41ccefa1b3443f2fb782e0432a997cb
Dennis Neumann <43755846+Dennis2512@users.noreply.github.com>
2020-09-24 22:44:15 +0200

    Added Stylesheet and Mock-Chart-Data

--- FrontendProj/src/app/caloriecounter/caloriecounter.component.ts
+++ FrontendProj/src/app/caloriecounter/caloriecounter.component.ts
@@ -5,8 +5,21 @@ import * as app from "tns-core-modules/application";
 @Component({
     selector: "Caloriecounter",
     templateUrl: "./caloriecounter.component.html",
+    styleUrls: ["./home.component.css"]
 })
 export class CaloriecounterComponent implements OnInit {
+    mealSource: { Meal: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
+        { Meal: "Breakfast", Amount: 15, SecondVal: 14, ThirdVal: 24 },
+        { Meal: "Lunch", Amount: 13, SecondVal: 23, ThirdVal: 25 },
+        { Meal: "Dinner", Amount: 24, SecondVal: 17, ThirdVal: 23 }
+    ];
+
+    weightSource: { Month: string, Amount: number, SecondVal: number, ThirdVal: number }[] = [
+        { Month: "Jan", Amount: 15, SecondVal: 14, ThirdVal: 24 },
+        { Month: "Feb", Amount: 13, SecondVal: 23, ThirdVal: 25 },
+        { Month: "Mar", Amount: 24, SecondVal: 17, ThirdVal: 23 }
+    ];
+    
     constructor() {
         // Use the component constructor to inject providers.
     }
