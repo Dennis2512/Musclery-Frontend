4d28036768e29ab40d812fc672c13d99c1ab523e
Dennis Neumann <43755846+Dennis2512@users.noreply.github.com>
2020-09-24 22:45:55 +0200

    Update caloriecounter.module.ts

--- FrontendProj/src/app/caloriecounter/caloriecounter.module.ts
+++ FrontendProj/src/app/caloriecounter/caloriecounter.module.ts
@@ -2,10 +2,11 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { CaloriecounterRoutingModule } from "./caloriecounter-routing.module";
+import { NativeScriptUIChartModule } from "nativescript-ui-chart/angular";
 import { CaloriecounterComponent } from "./caloriecounter.component";
 
 @NgModule({
-    imports: [NativeScriptCommonModule, CaloriecounterRoutingModule],
+    imports: [NativeScriptCommonModule, CaloriecounterRoutingModule, NativeScriptUIChartModule],
     declarations: [CaloriecounterComponent],
     schemas: [NO_ERRORS_SCHEMA],
 })
