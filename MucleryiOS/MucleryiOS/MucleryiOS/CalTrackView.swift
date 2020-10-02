59c82a803bf4093b281ca15578b4341afd9299c8
Arthur Siebert <arthursiebert@arthurs-imac.lan>
2020-10-02 16:05:54 +0200

    CalTrackView Visuals final

--- MucleryiOS/MucleryiOS/MucleryiOS/CalTrackView.swift
+++ MucleryiOS/MucleryiOS/MucleryiOS/CalTrackView.swift
@@ -12,6 +12,12 @@ struct CalTrackView: View {
     @State private var targetCal = 2400
     @State private var currentCal = 0
     @State private var leftCal = 2400
+    @State private var breakfast = 0
+    @State private var breakfastCal = 900
+    @State private var lunch = 0
+    @State private var lunchCal = 0
+    @State private var dinner = 0
+    @State private var dinnerCal = 0
     var body: some View {
         VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
             Rectangle()
@@ -28,7 +34,7 @@ struct CalTrackView: View {
             ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, content: {
                 Rectangle()
                     .foregroundColor(.clear)
-                //1
+                //Date + Cal
                 RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.circular/*@END_MENU_TOKEN@*/)
                     .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                     .foregroundColor(.white)
@@ -93,15 +99,102 @@ struct CalTrackView: View {
                             Spacer()
                     }))
                     .offset(y: -4)
-                //2
+                //Fruehstueck
                 RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
                     .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                     .foregroundColor(.white)
-                    .frame(width: 400.0, height: 200.0)
+                    .frame(width: 400.0, height: 100.0)
                     .overlay(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
                                 .stroke()
                                 .foregroundColor(.gray)
                                 .padding(.top, 0))
+                    .overlay(VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 220, content: {
+                            Text("Fruehstueck")
+                                .foregroundColor(.black)
+                            Text("\(breakfastCal)")
+                        })
+                        //Nahrungsmittelliste
+                        //Text("Placeholder")
+                        //End
+                        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                                .padding(.bottom, -2.0)
+                                .padding(.top, 2.0)
+                                .foregroundColor(.gray)
+                                .frame(width: 370.0, height: 37.0)
+                                .overlay(HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                                    Text("Nahrungsmittel Hinzufuegen")
+                                        .offset(y: 2)
+                                        .foregroundColor(.white)
+                                }))
+                                .offset(y: 7)
+                        }
+                    }))
+                //Mittagessen
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .frame(width: 400.0, height: 100.0)
+                    .overlay(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                .stroke()
+                                .foregroundColor(.gray)
+                                .padding(.top, 0))
+                    .overlay(VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 250, content: {
+                            Text("Mittagessen")
+                                .foregroundColor(.black)
+                            Text("\(lunchCal)")
+                        })
+                        //Nahrungsmittelliste
+                        //Text("Placeholder")
+                        //End
+                        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                                .padding(.bottom, -2.0)
+                                .padding(.top, 2.0)
+                                .foregroundColor(.gray)
+                                .frame(width: 370.0, height: 37.0)
+                                .overlay(HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                                    Text("Nahrungsmittel Hinzufuegen")
+                                        .offset(y: 2)
+                                        .foregroundColor(.white)
+                                }))
+                                .offset(y: 7)
+                        }
+                    }))
+                //Abendessen
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .frame(width: 400.0, height: 100.0)
+                    .overlay(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                                .stroke()
+                                .foregroundColor(.gray)
+                                .padding(.top, 0))
+                    .overlay(VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 250, content: {
+                            Text("Abendessen")
+                                .foregroundColor(.black)
+                            Text("\(dinnerCal)")
+                        })
+                        //Nahrungsmittelliste
+                        //Text("Placeholder")
+                        //End
+                        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                                .padding(.bottom, -2.0)
+                                .padding(.top, 2.0)
+                                .foregroundColor(.gray)
+                                .frame(width: 370.0, height: 37.0)
+                                .overlay(HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                                    Text("Nahrungsmittel Hinzufuegen")
+                                        .offset(y: 2)
+                                        .foregroundColor(.white)
+                                }))
+                                .offset(y: 7)
+                        }
+                    }))
             })
             .offset(y: -58)
             Spacer()
